7c253abf9ad9cb09c40fa1faa66745da2d80c676
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2020-10-17 04:48:54 -0300

    adiciona validação de dados

--- aulas/backend/src/controllers/OrphanagesController.ts
+++ aulas/backend/src/controllers/OrphanagesController.ts
@@ -2,7 +2,7 @@ import {Request,Response} from 'express';
 import {getRepository} from 'typeorm';
 import Orphanage from '../models/Orphanage'
 import OrphanageView from '../views/orphanage_view'
-
+import * as Yup  from 'yup';
 
 export default {
 
@@ -38,7 +38,7 @@ export default {
 
 
     /**
-     * @description método para criar um orfanato
+     * @description método p ara criar um orfanato
      * @param {Request} request 
      * @param {Response} response 
      */ 
@@ -61,18 +61,38 @@ export default {
             return {path: image.filename }
         })
 
-        const orphanage = orphanagesRepository.create(
-            {
-                name,
-                latitude,
-                longitude,
-                about,
-                instructions,
-                opening_hours,
-                open_on_weekends,
-                images
-            }
-        );
+
+        const data =  {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+            images
+        }
+    
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(), 
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
+        })
+
+        await schema.validate(data,{
+            abortEarly: false,
+        })
+
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
         return response.status(201).json(orphanage);
--- aulas/backend/src/server.ts
+++ aulas/backend/src/server.ts
@@ -1,14 +1,18 @@
-
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
 import 'express-async-errors';
 import './database/connection';
+
 import routes from './routes'; 
-import  errorHandler from './errors/handler';
+import errorHandler from './errors/handler';
 
 const app = express ();
-
+app.use(cors());
 app.use(express.json()) //adiciona suporte ao app suporte a JSON
 app.use(routes);
+app.use('/uploads',express.static(path.join(__dirname,'..','uploads')))
 app.use(errorHandler);
 //configura rotas para criar orfanato (metodo post)
 
